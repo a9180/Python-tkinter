1b549cb23decd37076c019c58d9d0f9f63876411
a9180 <santos888venancio@gmail.com>
2024-09-29 09:29:38 -0400

    Update

--- Estudando Pytyhon/Curso de python/scrpt1.py
+++ Estudando Pytyhon/Curso de python/scrpt1.py
@@ -1,3 +1,50 @@
 # OPERADORES
 
 # atribuição
+# valor = 2 
+# valor += 2
+# valor -= 2
+# valor *= 2
+# valor /= 2
+# valor %= 2
+
+# comparação
+#valor = 10
+#valor1 = 20
+
+#valor == valor1
+#valor != valor1
+#valor > valor1
+#valor < valor1
+#valor >= valor1
+#valor <= valor1#
+
+# PYTHON LISTS - COLEÇÕES DE DADOS
+
+# LIST - coleção ordenada, alterável e que permite valores duplicados
+lista = ['Andre', 'ASde', 'erwer']
+#print(lista)
+# apresentar valor
+#print(lista[0])
+
+# alterar valor
+#lista[2] = 'asdf'
+#print(lista[2])
+
+# iteração por todos os valores
+#for nome in lista:
+#    print(nome)
+
+# verificar se um item existe na lista
+#if "Andre" in lista:
+#    print('Existe!')
+#else:
+#   print('|Não exite')
+
+# quantidade de elementos numa lista
+#print(len(lista))
+
+# adicionar itens
+# lista.append('V!@sssdfsd')
+# print(len(lista))
+# print(lista)
